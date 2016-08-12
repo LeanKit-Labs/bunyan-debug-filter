e727dd9a6b1fc92ed74b5b0b5e7ac181d5024f15
Matt Dunlap <matt.dunlap@leankit.com>
2016-08-12 14:01:20 -0600

    Fixed issue with installs in some projects

--- tasks/symlink-spec-helpers.js
+++ tasks/symlink-spec-helpers.js
@@ -3,9 +3,13 @@
 const fs = require( "fs" );
 const dstpath = "node_modules/spec-helpers"; // the symlink
 const srcpath = "../spec-helpers"; // the src path relative to node_modules
-fs.exists( dstpath, function( exists ) {
-	// create the link only if it does not already exist
-	if ( !exists ) {
-		fs.symlinkSync( srcpath, dstpath, "dir" );
+fs.exists( "./node_modules", function( hasNodeModules ) {
+	if ( hasNodeModules ) {
+		fs.exists( dstpath, function( exists ) {
+			// create the link only if it does not already exist
+			if ( !exists ) {
+				fs.symlinkSync( srcpath, dstpath, "dir" );
+			}
+		} );
 	}
 } );
